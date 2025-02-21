f88cca130ba716ddf138f51ea630f0cc528070c8
13750404 <yixiangceng74@gmail.com>
2025-02-21 12:14:04 +0800

    Add files via upload

---
+++ week01-1.cpp
@@ -0,0 +1,12 @@
+/// weel01-1.cpp 要自己打好全部檔名, 後面要打.cpp
+/// 在瘋狂程設(練習模式)寫好的程式碼, 全選, Ctrl-C
+/// CodeBlocks File-NewEmpty 空白檔案, Ctrl-S存檔
+#include <stdio.h> ///使用standard 標準的 input/output
+
+int main()
+{
+	int a, b; ///整數a, b
+	scanf("%d%d",&a, &b); ///讀入資料 小心 "%d%d" 後面[逗號]
+	/// 兩個%d%d & &
+	printf("%d",a+b); ///印出資料, 把答案印出來
+}
